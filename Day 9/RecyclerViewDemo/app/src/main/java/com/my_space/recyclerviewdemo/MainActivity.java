c84c80d0166d2a447961a1c536869d36e221c414
crsohan4 <sohan.m.kulkarni@gmail.com>
2021-09-23 20:02:16 +0530

    Source code of day 10

--- Day 9/RecyclerViewDemo/app/src/main/java/com/my_space/recyclerviewdemo/MainActivity.java
+++ Day 9/RecyclerViewDemo/app/src/main/java/com/my_space/recyclerviewdemo/MainActivity.java
@@ -1,14 +1,51 @@
 package com.my_space.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.my_space.recyclerviewdemo.adapter.MyAdapter;
+import com.my_space.recyclerviewdemo.model.MyModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<MyModel> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Dummy data created **start**
+        listItems = new ArrayList<>();
+
+        for(int i=0; i<10; i++){
+//            MyModel myModel = new MyModel(
+//                    "Item " + (i + 1),
+//                        "Description " + (i + 1)
+//                    );
+            MyModel myModel = new MyModel();
+            myModel.setTitle("Item " + (i + 1));
+            myModel.setDescription("Description " + (i + 1));
+
+            listItems.add(myModel);
+        }
+        // Dummy data created **end**
+
+        adapter = new MyAdapter(this, listItems);
+        recyclerView.setAdapter(adapter);
+
     }
 }
--- Day 9/RecyclerViewDemo/app/src/main/java/com/my_space/recyclerviewdemo/adapter/MyAdapter.java
+++ Day 9/RecyclerViewDemo/app/src/main/java/com/my_space/recyclerviewdemo/adapter/MyAdapter.java
@@ -1,15 +1,19 @@
 package com.my_space.recyclerviewdemo.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.my_space.recyclerviewdemo.R;
+import com.my_space.recyclerviewdemo.SecondActivity;
 import com.my_space.recyclerviewdemo.model.MyModel;
 
 import java.util.List;
@@ -37,6 +41,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         holder.titleTv.setText(item.title);
         holder.descriptionTv.setText(item.description);
+
+        holder.titleTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, item.getTitle() + " is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.descriptionTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, item.description + " is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, item.getTitle() + " card is clicked!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, SecondActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -47,11 +74,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView titleTv, descriptionTv;
+        public CardView cardView;
         public ViewHolder(View itemView) {
             super(itemView);
 
             titleTv = itemView.findViewById(R.id.title);
             descriptionTv = itemView.findViewById(R.id.description);
+            cardView = itemView.findViewById(R.id.cardView);
         }
     }
 }
